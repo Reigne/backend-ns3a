3dee132f36db4d55eb1db01f04716d8f021796b7
Reigne <monteronaelija@yahoo.com>
2023-03-29 11:20:26 +0700

    ADD CORD URL

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({limit: "100mb", extended: true }));
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://shopit-backend-797m.onrender.com",
     credentials: true
 }));
 
